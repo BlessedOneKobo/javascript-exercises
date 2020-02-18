0591e82c450faa4ef568a0e3bae494d6afc49885
BlessedOneKobo <BlessedOneKobo@protonmail.com>
2020-02-18 11:04:22 +0100

    Complete removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+  const arrCopy = arr.slice();
 
+  items.forEach((item) => {
+    const itemIndex = arrCopy.indexOf(item);
+
+    if (itemIndex !== -1) {
+      arrCopy.splice(itemIndex, 1);
+    }
+  });
+
+  return arrCopy;
 }
 
 module.exports = removeFromArray
