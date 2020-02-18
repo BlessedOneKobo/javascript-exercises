9fc9bdc6fdff6393589e6584a2446f0b4322722d
BlessedOneKobo <BlessedOneKobo@protonmail.com>
2020-02-18 11:12:15 +0100

    Complete sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  // Handle non-number parameters
+  if (typeof start !== 'number' || typeof end !== 'number') {
+    return 'ERROR';
+  }
 
+  // Handle negative numbers
+  if (start < 0 || end < 0) {
+    return 'ERROR';
+  }
+
+  // Handle scenario when a larger number is the first parameter
+  if (start > end) {
+    const temp = start;
+    start = end;
+    end = temp;
+  }
+  
+  let sum = 0;
+  for (let num = start; num <= end; num++) {
+    sum += num;
+  }
+
+  return sum;
 }
 
 module.exports = sumAll
