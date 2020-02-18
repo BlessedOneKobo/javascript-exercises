c428bd1d18baf83f6523d618a9e16eff6bde0208
BlessedOneKobo <BlessedOneKobo@protonmail.com>
2020-02-18 12:06:38 +0100

    Complete tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  return round((fahrenheit - 32) * 5 / 9, 1);
 }
 
-const ctof = function() {
+const ctof = function(celsius) {
+  return round(celsius * 9 / 5 + 32, 1);
+}
 
+const round = function(num, dp = 1) {
+  if (dp < 0) {
+    dp = 0;
+  }
+  
+  const mult = 10 ** dp;
+  return Math.round(num * mult) / mult;
 }
 
 module.exports = {
