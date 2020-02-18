41d3090fc8cf857e8a68ea98e1ae9f359e2f911a
BlessedOneKobo <blessedonekobo@protonmail.com>
2020-02-18 06:31:55 +0100

    Complete repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, repetitions) {
+  if (repetitions < 0) {
+    return 'ERROR';
+  }
 
+  let strRepeated = '';
+
+  for (let i = 0; i < repetitions; i++) {
+    strRepeated += str;
+  }
+
+  return strRepeated;
 }
 
 module.exports = repeatString
