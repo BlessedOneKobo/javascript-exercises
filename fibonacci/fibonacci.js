25eceec3568f75e6180da7fc2ef892ac384ed557
BlessedOneKobo <BlessedOneKobo@protonmail.com>
2020-02-19 12:22:40 +0100

    Complete fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(lim) {
+  lim = Number(lim);
 
+  // Handle negative numbers and strings
+  if (lim < 0 || Number.isNaN(lim)) {
+    return 'OOPS';
+  }
+
+  let a = 0;
+  let b = 1;
+  for (let i = 0; i < lim; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+
+  return a;
 }
 
 module.exports = fibonacci
