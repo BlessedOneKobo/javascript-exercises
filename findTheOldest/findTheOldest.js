c691ec24d262a11652e8e6f06924ded941bcff33
BlessedOneKobo <BlessedOneKobo@protonmail.com>
2020-02-19 13:29:12 +0100

    Complete findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const ageForOldestPerson = calculateAge(oldestPerson);
+    const ageForCurrentPerson = calculateAge(currentPerson);
 
+    if (ageForCurrentPerson > ageForOldestPerson) {
+      return currentPerson;
+    }
+
+    return oldestPerson;
+  });
+}
+
+function calculateAge(personObj) {
+  // Handle dead people
+  if (personObj.yearOfDeath) {
+    return personObj.yearOfDeath - personObj.yearOfBirth;
+  }
+
+  // Handle living people
+  return (new Date()).getFullYear() - personObj.yearOfBirth;
 }
 
 module.exports = findTheOldest
